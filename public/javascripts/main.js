a8c9beca5850bf105aef5618ebe06a1076823e15
jaewoongh <jaewoong.here@gmail.com>
2014-04-11 23:41:31 -0400

    replaced client side socketwork into sample code

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -22,38 +22,46 @@
 // 	};
 // })();
 
-var connection = (function() {
-	var ws, host;
-
-	return {
-		init: function() {
-			host = location.origin.replace(/^http/, 'ws');
-			ws = new WebSocket(host);
-			
-			ws.onopen = function() {
-				this.send(JSON.stringify({ message: 'Hello server!' }));
-			};
-
-			ws.onmessage = function(evt) {
-				console.log(JSON.parse(evt.data));
-			};
-
-			return this;
-		},
-
-		send: function(msg, onErr) {
-			try {
-				ws.send(JSON.stringify(msg));
-			} catch(e) {
-				(onErr || function() {
-					console.log(e);
-				})();
-			}
-			return this;
-		}
-	};
-})();
-
-window.onload = function() {
-	connection.init();
+var host = location.origin.replace(/^http/, 'ws')
+var ws = new WebSocket(host);
+ws.onmessage = function (event) {
+	var li = document.createElement('li');
+	li.innerHTML = JSON.parse(event.data);
+	// document.appendChild(li);
 };
+
+// var connection = (function() {
+// 	var ws, host;
+
+// 	return {
+// 		init: function() {
+// 			host = location.origin.replace(/^http/, 'ws');
+// 			ws = new WebSocket(host);
+			
+// 			ws.onopen = function() {
+// 				this.send(JSON.stringify({ message: 'Hello server!' }));
+// 			};
+
+// 			ws.onmessage = function(evt) {
+// 				console.log(JSON.parse(evt.data));
+// 			};
+
+// 			return this;
+// 		},
+
+// 		send: function(msg, onErr) {
+// 			try {
+// 				ws.send(JSON.stringify(msg));
+// 			} catch(e) {
+// 				(onErr || function() {
+// 					console.log(e);
+// 				})();
+// 			}
+// 			return this;
+// 		}
+// 	};
+// })();
+
+// window.onload = function() {
+// 	connection.init();
+// };
