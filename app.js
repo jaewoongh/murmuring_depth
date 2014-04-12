4a42fd69d893aed330498be9483cb4ffc05810b0
jaewoongh <jaewoong.here@gmail.com>
2014-04-12 00:02:41 -0400

    changed static dir

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/'));
 
 // app.use('/', routes);
 // app.use('/users', users);
